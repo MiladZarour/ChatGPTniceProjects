5a0a2df91256fab46bccac2bce7e7d4455e65120
Milad Zarour <Miladzarour@hotmail.com>
2023-04-19 19:49:35 +0200

    added a new fix

--- Youtube_File_Transcript_words/format_transcript.py
+++ Youtube_File_Transcript_words/format_transcript.py
@@ -1,4 +1,5 @@
 import sys
+import pickle
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
